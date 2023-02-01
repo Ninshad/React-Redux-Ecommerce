db808e226539ca6cd5fdc979cd9e8c856636a906
Ninshad <ninshad7@gmail.com>
2023-02-01 17:39:28 +0530

    1/2/23

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Slider from '../../components/Slider/Slider'
+import Categories from '../../components/Categories/Categories'
 import Contact from '../../components/Contact/Contact'
 import "./Home.scss"
 const Home = () => {
   return (
     <div className='home'>
       <Slider/>
+      <Categories/>
       <Contact/>
     </div>
   )
