8a19e1043456e66de4772d81baf2e9c1b3d973b1
Ninshad <ninshad7@gmail.com>
2023-02-02 18:05:26 +0530

    2/2/23

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Slider from '../../components/Slider/Slider'
 import Categories from '../../components/Categories/Categories'
 import Contact from '../../components/Contact/Contact'
+import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
 import "./Home.scss"
 const Home = () => {
   return (
     <div className='home'>
       <Slider/>
+      <FeaturedProducts type="featured"/>
       <Categories/>
       <Contact/>
     </div>
